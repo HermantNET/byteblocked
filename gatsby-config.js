e6df67088bd780cda36ef34299aa052dfdb14d83
HermantNET <hermant.edu@gmail.com>
2018-05-30 04:19:32 +0800

    Added metadata, installed nebulas and nebpay

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
     title: 'Byteblocked',
+    description:
+      'A repository of modular DApps for the Nebulas platform. Byteblocked hosts a collection of smart contracts to enhance blockchain development.',
+    tags:
+      'nebulas, blockchain, nas, dapp, distributed, application, app, nebulasio, nebulearn',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
